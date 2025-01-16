e93016608ac567af05ad470a84b5db248d3e49cd
FernandoDerentiis <derentiisfernando@gmail.com>
2025-01-16 19:01:09 +0100

    feat: calcolo prezzo

--- script.js
+++ script.js
@@ -22,4 +22,19 @@ document
       "POCIE24",
     ];
     console.log("Codici promozionali validi:", codiciPromozionaliValidi);
+
+    // / Imposta  orario in base al lavoro scelto
+    let prezzoOrario = 0;
+    if (tipoDiLavoro === "full-time") {
+      prezzoOrario = 20.5;
+    } else if (tipoDiLavoro === "part-time") {
+      prezzoOrario = 15.3;
+    } else {
+      prezzoOrario = 33.6;
+    }
+    console.log("Prezzo orario:", prezzoOrario);
+
+    // Calcola il prezzo totale per 10 ore
+    let prezzoFinale = prezzoOrario * 10;
+    console.log("Prezzo iniziale (10 ore):", prezzoFinale);
   });
